79062d4f83aaa45b8ffbaba1d333771c2950a9b0
Bhis10 <bhisma.mgr@gmail.com>
2021-07-17 22:18:21 +0100

    navscroll to section

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const tabs=document.querySelectorAll('[data-tab-target]')
 const tabContents=document.querySelectorAll('[data-tab-content]')
-
+const tv=document.querySelectorAll('tv')
 tabs.forEach(tab => {
     tab.addEventListener('click',()=>{
         const target=document.querySelector(tab.dataset.tabTarget)
@@ -9,4 +9,12 @@ tabs.forEach(tab => {
 })
 target.classList.add('active')
 })
+})
+tv.forEach((item)=>{
+    item.addEventListener('click',()=>
+    {
+        let el=document.getElementById(item.getAttribute('tv'));
+        el.scrollIntoView();
+
+    })
 })
